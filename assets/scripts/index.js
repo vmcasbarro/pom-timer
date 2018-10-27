f95b60ce7909e12925b852b4cc5efc3d9b07028a
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-10-27 15:23:27 -0400

    pom timer

---
+++ assets/scripts/index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+let pomIntervalId
+
+function notifyUser() {
+  alert('timer is done!')
+}
+
+function resetTimer() {
+  clearInterval(pomIntervalId)
+}
+
+function startTimer(durationInSeconds) {
+  resetTimer()
+  let timer = durationInSeconds
+  pomIntervalId = setInterval( () => {
+    const minutes = parseInt(timer/60, 10)
+    const seconds = parseInt(timer%60, 10)
+
+    let clockMinutes = minutes < 10 ? '0'+minutes : minutes
+    let clockSeconds = seconds < 10 ? '0'+seconds : seconds
+
+    $('.countdown').html(`${clockMinutes}:${clockSeconds}`)
+
+    --timer
+
+    // what is this really doing?
+    // if (--timer < 0 ) {
+    //   timer = durationInSeconds
+    // }
+
+    if (minutes == 0 && seconds == 0) {
+      notifyUser()
+      resetTimer()
+    }
+  }, 1000)
+}
+
+$(() => {
+  $('#five').on('click', () => {
+    startTimer(300)
+  })
+  $('#fifteen').on('click', () => {
+    startTimer(900)
+  })
+  $('#twentyfive').on('click', () => {
+    startTimer(1500)
+  })
+  $('#reset').on('click', () => {
+    resetTimer()
+    $('.countdown').html('00:00')
+  })
+})
